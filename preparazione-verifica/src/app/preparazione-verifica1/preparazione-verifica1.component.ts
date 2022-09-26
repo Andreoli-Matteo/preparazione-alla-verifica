3ffd243a5215596eba557e2fdb76a62742b2f7c6
Andreoli-Matteo <andreoli.matteo@iisgalvanimi.edu.it>
2022-09-26 09:58:45 +0000

    preparazione alla verifica

---
+++ preparazione-verifica/src/app/preparazione-verifica1/preparazione-verifica1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preparazione-verifica1',
+  templateUrl: './preparazione-verifica1.component.html',
+  styleUrls: ['./preparazione-verifica1.component.css']
+})
+export class PreparazioneVerifica1Component implements OnInit {
+   title="quarto";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
